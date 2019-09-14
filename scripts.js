6c86627f4e032bf434b8ee3cf42f464fb8b5dbd0
David Cid <david.cid@gmail.com>
2019-09-15 01:19:09 +0200

    filter by status and name

--- scripts.js
+++ scripts.js
@@ -1,34 +1,51 @@
 const rickAndMortyApi = 'https://rickandmortyapi.com/api/character/';
-  const characters = [];
+const characters = [];
+let status = document.querySelector('input[type=radio]:checked').value;
+let wordToMatch = '';
 
-  fetch(rickAndMortyApi)
-    .then(res => res.json())
-    .then(data => characters.push(...data.results));
+fetch(rickAndMortyApi)
+  .then(res => res.json())
+  .then(data => characters.push(...data.results));
 
-  function findMatches(wordToMatch, characters) {
-    return characters.filter(character => {
-      // here we need to figure out if the character matches what was searched
-      const regex = new RegExp(wordToMatch, 'gi');
-      return character.name.match(regex);
-    })
-  }
+function findMatches(wordToMatch, status, characters) {
+  return characters.filter(character => {
+    // here we need to figure out if the character matches what was searched
+    const regex = new RegExp(wordToMatch, 'gi');
+    // return character.name.match(regex);
+    const statusRegex = new RegExp(status, 'gi');
+    return (character.name.match(regex) && character.status.match(statusRegex));
+  })
+}
 
-  function displayMatches() {
-    const matchArray = findMatches(this.value, characters);
-    const html = matchArray.map(character => {
-      // const regex = new RegExp(this.value, 'gi');
-      const characterName = character.name;
-      return `
-        <li>
-          <span class="name">${characterName}</span>
-        </li>
-      `;
-    }).join('');
-    result.innerHTML = html;
-  }
+function displayMatches() {
+  const matchArray = findMatches(wordToMatch, status, characters);
+  const html = matchArray.map(character => {
+    return `
+      <li>
+        <img class="image" src=${character.image} alt="image">
+        <h3 class="name">${character.name}</h3>
+        <p class="status">${character.status}</p>
+      </li>
+    `;
+  }).join('');
+  result.innerHTML = html;
+}
 
-  const searchInput = document.querySelector('.search');
-  const result = document.querySelector('.characters');
+function updateStatus() {
+  this.checked = true;
+  status = this.value;
+  displayMatches();
+}
 
-  searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', displayMatches);
+function updateWordToMatch() {
+  wordToMatch = this.value;
+  displayMatches();
+}
+
+const searchInput = document.querySelector('.search');
+const result = document.querySelector('.characters');
+const statusButtons = document.querySelectorAll('input[type=radio]');
+
+searchInput.addEventListener('change', updateWordToMatch);
+searchInput.addEventListener('keyup', updateWordToMatch);
+statusButtons.forEach(button => button.addEventListener('click', updateStatus));
