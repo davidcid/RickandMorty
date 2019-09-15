41feb3e6977e3c4da6d62d8cf5ead70d939dede3
David Cid <david.cid@gmail.com>
2019-09-15 23:26:10 +0200

    fixing multiple pages requests

--- scripts.js
+++ scripts.js
@@ -1,28 +1,46 @@
 const rickAndMortyApi = 'https://rickandmortyapi.com/api/character/';
-const characters = [];
-let status = document.querySelector('input[type=radio]:checked').value;
+
+const searchInput =   document.querySelector('.search');
+const result =        document.querySelector('.characters');
+const statusButtons = document.querySelectorAll('input[type=radio]');
+
+let status =          document.querySelector('input[type=radio]:checked').value;
 let wordToMatch = '';
 
-for (var i = 0; i < 26; i++) {
-  fetch(rickAndMortyApi + "?page=" + i)
-  .then(res => res.json())
-  .then(data => characters.push(...data.results))
-  .catch(err => console.log("Personaje no encontrado"))
-};
 
-function findMatches(wordToMatch, status, characters) {
-  return characters.filter(character => {
-    // here we need to figure out if the character matches what was searched
-    const regex = new RegExp(wordToMatch, 'gi');
-    // return character.name.match(regex);
-    const statusRegex = new RegExp(status, 'gi');
-    return (character.name.match(regex) && character.status.match(statusRegex));
+function getData(wordToMatch, status) {
+  fetch(`${rickAndMortyApi}?page=1&name=${wordToMatch}&status=${status}`)
+  .then(res => res.json())
+  .then(data => {
+    let characters = data.results;
+    const totalPages = data.info.pages;
+
+    if(totalPages > 1) {
+      for(let page = 2; page <= totalPages; page++) {
+        fetch(`${rickAndMortyApi}?page=${page}&name=${wordToMatch}&status=${status}`)
+          .then(res => res.json())
+          .then(data => {
+            // characters = characters.concat(data.results);
+            characters.push(...data.results);
+            if(page === totalPages) {
+              displayMatches(characters);
+            }
+          })
+          .catch(err => {
+            result.innerHTML = `<p class="no-results">No se han encontrado resultados</p>`;
+          })
+      }
+    } else {
+      displayMatches(characters);
+    }
+  })
+  .catch(err => {
+    result.innerHTML = `<p class="no-results">No se han encontrado resultados</p>`;
   })
 }
 
-function displayMatches() {
-  const matchArray = findMatches(wordToMatch, status, characters);
-  const html = matchArray.map(character => {
+function displayMatches(characters = []) {
+    const html = characters.map(character => {
     return `
       <li>
         <img class="image" src=${character.image} alt="image">
@@ -37,20 +55,18 @@ function displayMatches() {
 function updateStatus() {
   this.checked = true;
   status = this.value;
-  displayMatches();
+  getData(wordToMatch, status);
 }
 
 function updateWordToMatch() {
   wordToMatch = this.value;
-  displayMatches();
+  const myVar = setTimeout(getData(wordToMatch, status), 500);
+  console.log(wordToMatch);
 }
 
-const searchInput = document.querySelector('.search');
-const result = document.querySelector('.characters');
-const statusButtons = document.querySelectorAll('input[type=radio]');
+
+
 
 searchInput.addEventListener('change', updateWordToMatch);
 searchInput.addEventListener('keyup', updateWordToMatch);
 statusButtons.forEach(button => button.addEventListener('click', updateStatus));
-
-displayMatches();
