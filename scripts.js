542cb215b5473e03de137fb7d13e515f18bcccb0
David Cid <david.cid@gmail.com>
2019-09-17 17:54:17 +0200

    Working on expanded card

--- scripts.js
+++ scripts.js
@@ -1,44 +1,11 @@
 const rickAndMortyApi = 'https://rickandmortyapi.com/api/character/';
 
-const searchInput =   document.querySelector('.search');
-const result =        document.querySelector('.characters');
-const statusButtons = document.querySelectorAll('input[type=radio]');
-
-
-let status =          document.querySelector('input[type=radio]:checked').value;
+const searchInput =       document.querySelector('.search');
+const result =            document.querySelector('.characters');
+const statusButtons =     document.querySelectorAll('input[type=radio]');
+let status =              document.querySelector('input[type=radio]:checked').value;
+const characterExpanded = document.querySelector('.character-expanded');
 let wordToMatch = 'morty';
-
-
-// function getData(wordToMatch, status) {
-//   fetch(`${rickAndMortyApi}?page=1&name=${wordToMatch}&status=${status}`)
-//   .then(res => res.json())
-//   .then(data => {
-//     let characters = data.results;
-//     const totalPages = data.info.pages;
-
-//     if(totalPages > 1) {
-//       for(let page = 2; page <= totalPages; page++) {
-//         fetch(`${rickAndMortyApi}?page=${page}&name=${wordToMatch}&status=${status}`)
-//           .then(res => res.json())
-//           .then(data => {
-//             // characters = characters.concat(data.results);
-//             characters.push(...data.results);
-//             if(page === totalPages) {
-//               displayMatches(characters);
-//             }
-//           })
-//           .catch(err => {
-//             result.innerHTML = `<p class="no-results">No se han encontrado resultados</p>`;
-//           })
-//       }
-//     } else {
-//       displayMatches(characters);
-//     }
-//   })
-//   .catch(err => {
-//     result.innerHTML = `<p class="no-results">No se han encontrado resultados</p>`;
-//   })
-// }
 
 async function getData() {
   try {
@@ -80,7 +47,7 @@ function displayMatches(characters = []) {
       `;
     }).join('');
   result.innerHTML = html;
-  const cards =         document.querySelectorAll('.card');
+  const cards = document.querySelectorAll('.card');
   cards.forEach(card => card.addEventListener('click', showDetails));
 }
 
@@ -92,25 +59,41 @@ function updateStatus() {
 
 function updateWordToMatch() {
   wordToMatch = this.value;
-  setTimeout(getData, 5000);
+  setTimeout(getData, 800);
 }
 
 function showDetails() {
   id = this.querySelector('.id').innerHTML;
-  console.log(id);
-  getCharacterData();
+  expanded = document.querySelector(".expanded") || '';
+  if(expanded !== '') {
+    expanded.classList.remove("expanded");
+  }
+  this.classList.add("expanded");
+  data = getCharacterData();
 }
 
 async function getCharacterData() {
   try {
     let response = await fetch(`${rickAndMortyApi}${id}`);
     let data = await response.json();
-    console.log(data);
+    showCharacter(data);
   }
   catch(error) {
     result.innerHTML = `<p class="no-results">No se han encontrado resultados</p>`;
   }    
 }
+
+function showCharacter(character) {
+  let html = `
+    <div class="card-expanded">
+      <h3>${character.name}</h3>
+    </div>
+  `
+  characterExpanded.innerHTML = html;
+  console.log(character.name);
+}
+
+
 
 getData();
 
